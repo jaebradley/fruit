06c7e4049fbfc5fd087850be16167fc991b6b5d0
jaebradley <jae.b.bradley@gmail.com>
2018-04-19 16:18:54 -0400

    fix(templates): minor template updates

---
+++ src/templates/node/rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import localResolve from 'rollup-plugin-local-resolve';
+import filesize from 'rollup-plugin-filesize';
+import minify from 'rollup-plugin-babel-minify';
+
+const config = {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'build/index.js',
+      format: 'umd',
+      name: {{name}},
+    },
+    {
+      file: 'build/index.cjs.js',
+      format: 'cjs',
+      name: {{name}},
+    },
+    {
+      file: 'build/index.esm.js',
+      format: 'es',
+    },
+  ],
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    minify(),
+    localResolve(),
+    resolve(),
+    commonjs(),
+    filesize(),
+  ],
+};
+
+export default config;
