18f45c4fb80d0517b56e9af3dc7bf9a188e2c68e
jaebradley <jae.b.bradley@gmail.com>
2018-04-20 09:31:50 -0400

    fix(build): rename package.json template

--- src/fileWriters/writeTemplateDirectoryFiles.js
+++ src/fileWriters/writeTemplateDirectoryFiles.js
@@ -7,7 +7,18 @@ const writeTemplateDirectoryFiles = async ({ location, destination, templateValu
   const files = await globby(location, { dot: true });
   files.forEach((file) => {
     const relativeFilePath = path.relative(location, file);
-    const destinationFilePath = path.join(destination, relativeFilePath);
+
+    let destinationFilePath = path.join(destination, relativeFilePath);
+    // (Ass)uming that package.json will always be at base of destination
+    // I can't wait until I regret hard-coding this
+
+    // I need to do this because I have templated package.json
+    // npm actually won't package directories with invalid package.json files
+    // so I have to change the name to package.template.json
+    if (file.indexOf('package.template.json') >= 0) {
+      destinationFilePath = path.join(destination, 'package.json');
+    }
+
     writeTemplateFile({
       templateValues,
       location: file,
